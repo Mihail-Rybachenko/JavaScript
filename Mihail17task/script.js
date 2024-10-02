9e32deaa128fad3b61b949f4d32c40f6b2a95fdb
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-02 20:32:29 +0300

    add task 17-18

---
+++ Mihail17task/script.js
@@ -0,0 +1,27 @@
+"use strict";
+let a = 1 + 2 + 3;
+alert(a);
+
+let a1 = 10;
+let b = 2;
+alert(a1+b)
+alert(a1-b)
+alert(a1*b)
+alert(a1/b)
+
+let c = 10;
+let d = 5;
+let result = c + d;
+alert(result)
+let a2, b2, c2;
+a2 = 1;
+b2 = 2;
+c2 = 3;
+alert(a2 + b2 + c2);
+let a3, b3, c3, d3, result1;
+a3 = 10;
+b3 = 5;
+c3 = a3 - b3;
+d3 = 7;
+result1 = c3 + d3;
+alert(result1)
