519248832504bc939cef9c59782903e94b7d64cf
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-03 19:21:53 +0300

    add task 28-30

---
+++ Mihail28-30task/script.js
@@ -0,0 +1,14 @@
+let Lastname = "Ribochenko";
+let name = "Mihail";
+alert(name+ Lastname);
+let str = "!!!";
+alert(str);
+let str2, str3, str4, str5, str6;
+str2 = "java";
+str3 = "script";
+alert(str2 + str3);
+str4 = "hello ";
+str5 = "world";
+alert(str4 + str5);
+str6 = "Mirius";
+alert(str6 .length);
