cc9aac4eec072a5d04e1fa6eff8f8750bab4429c
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-05 19:54:01 +0300

    add task 48-49

---
+++ Mihail48-49task/script.js
@@ -0,0 +1,8 @@
+let num1 = 5;
+let num2 = 4;
+let num3 = 9000;
+let num4 = 64;
+let num5 = 121;
+alert(String(num1) + String(num2));
+alert("49#1 " +String(num3).length);
+alert('49#2 ' + String(num4).length + String(num5).length);
