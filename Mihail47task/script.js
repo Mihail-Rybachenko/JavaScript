1dd867c3fe087f3339170a9bc45678e50b156f24
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-05 19:45:45 +0300

    add task 47

---
+++ Mihail47task/script.js
@@ -0,0 +1,8 @@
+let num, num1,num2,num3,num4,num5;
+num = parseFloat("5px");
+num1 = parseFloat("6px");
+num2 = parseFloat("5.5px");
+num3 = parseFloat("6.25px");
+alert(num + num1);
+alert(num2 + num3);
+alert(num2 + num3 + "px");
