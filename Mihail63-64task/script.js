50dfa3ad9d3b8f5f53f079df2c0c72b1dc718a6f
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-06 21:09:29 +0300

    add task 63-64

---
+++ Mihail63-64task/script.js
@@ -0,0 +1,21 @@
+"use strict";
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+
+alert(Number(num1) + Number(num2)); // сложит числа как числа
+let a = prompt("Введите сторону квадрата:");
+let square = Number(a) * Number(a);
+alert(square);
+let a1 = Number(prompt("Введите первую сторону:"));
+let b1 = Number(prompt("Введите вторую сторону:"));
+let P = (a1 + b1) * 2;
+alert(P);
+document.write("Mirius<br>");
+document.write('<i>Mirius</i> <br>');
+let str = 'text';
+document.write("<i>" + str + "</i> <br>");
+document.write("1<br>");
+document.write("2<br>");
+document.write("3<br>");
+document.write("4<br>");
+document.write("5<br>");
