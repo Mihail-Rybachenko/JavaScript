71740513cc7d296494add1bc14540a210795564d
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-06 14:09:22 +0300

    add task55-57

---
+++ Mihail55-57task/script.js
@@ -0,0 +1,8 @@
+let test = "12345";
+alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
+let number = String(12345);
+alert(Number(number[0]) + Number(number[1]) + Number(number[2]) + Number(number[3]) + Number(number[4]));
+let number2 = String(12345);
+alert(number2[0] * number2[1] * number2[2] * number2[3] * number2[4]);
+let number3 = String(12345);
+alert(number3[4] + number3[3] + number3[2] + number3[1] + number3[0]);
