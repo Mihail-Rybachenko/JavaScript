6bf459701c1527374526f2594d4a577a77971025
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-07 10:24:03 +0300

    add task 71-72

---
+++ massive/Mihail71-72task/script.js
@@ -0,0 +1,11 @@
+"use strict";
+let arr = ["a", "b", "c"];
+arr[0] = 1;
+arr[1] = -2;
+arr[2] = -3;
+alert(arr);
+let numbers = [1, 0, 7];
+numbers[0] += 3;
+numbers[1] += 3;
+numbers[2] += 3;
+console.log(numbers);
