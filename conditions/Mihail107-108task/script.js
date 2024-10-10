779af28542bd71fc6ef28ea04c53733f49f9c92c
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-10 20:01:08 +0300

    add task 107-108

---
+++ conditions/Mihail107-108task/script.js
@@ -0,0 +1,24 @@
+"use strict";
+console.log('107#1');
+let test = 1;
+if (test != 10){
+	console.log('+');
+}
+else{
+	console.log('-');
+}
+console.log('108#1');
+let test2 = 3;
+if (test2 > test){
+	console.log('+');
+}
+else{
+	console.log('-');
+}
+console.log('108#2');
+if (test2 == test){
+	console.log('+');
+}
+else{
+	console.log('-');
+}
