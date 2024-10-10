398d636786f21a1a3e2db81e53eba1045105376e
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-10 20:15:58 +0300

    add task 109-111

---
+++ conditions/Mihail109-111task/script.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log('109#1');
+let test1 = 'Mirius';
+let test2 = 'Mirius';
+if (test1 == test2){
+	console.log('yes');
+}
+else{
+	console.log('no')
+}
+console.log('110#1');
+let test3 = '123';
+let test4 = 123;
+if (test3 == test4){
+	console.log('yes');
+}
+else{
+	console.log('no')
+}
+
