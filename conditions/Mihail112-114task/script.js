0bda73bd040d79f86db5cd9bc8e89d4c3d0b432e
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-10 20:54:26 +0300

    add task112-114

---
+++ conditions/Mihail112-114task/script.js
@@ -0,0 +1,22 @@
+"use strict";
+console.log('113#1');
+let num = 3;
+let num1 = 2;
+let num2 = 3;
+if (num > 0 && num < 5) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+console.log('113#2');
+if (num >= 10 && num <= 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+console.log('113#3');
+if (num1 <= 1 && num2 >= 3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
