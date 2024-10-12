e3d6d7f6ad09058a6f37464a9e7faafc32528a82
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-12 19:37:00 +0300

    add task120-121task

---
+++ conditions/Mihail120-121task/script.js
@@ -0,0 +1,31 @@
+"use strict";
+console.log('120#1');
+let test = true;
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+console.log('121#1');
+let test1 = true;
+
+if (!test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+console.log('121#2');
+let test2 = true;
+
+if (!test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+console.log('121#3');
+let test3 = true;
+if (test3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
