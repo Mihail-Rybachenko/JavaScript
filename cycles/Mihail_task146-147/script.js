817286df8496c47c61584b022c963ebe55a0a67e
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-14 10:12:23 +0300

    add task146-147

---
+++ cycles/Mihail_task146-147/script.js
@@ -0,0 +1,46 @@
+"use strict";
+console.log('146#1')
+let num = 1;
+while(num < 101){
+    console.log(num);
+    num++;
+}
+console.log('146#2');
+let num1 = 11;
+while(num1 < 34){
+    console.log(num1)
+    num1++;
+}
+console.log('146#3');
+let num2 = 1;
+let sum = 0;
+while(num2 < 1001){
+    console.log(num2);
+    num2 *= 3;
+    sum++;
+}
+console.log('Число итераций: ' + sum);
+console.log('147#1');
+for (let i = 1; i <=100; i++){
+console.log(i);
+}
+console.log('147#2');
+for (let i = 11; i <=33; i++){
+    console.log(i);
+ }
+ console.log('147#3');
+ for (let i = 0; i <= 100; i++){
+if(i % 2 == 0){
+    console.log(i);
+}
+ }
+ console.log('147#4');
+ for (let i = 1; i <= 99; i++){
+    if(i % 2 != 0){
+        console.log(i);
+    }
+     }
+console.log('147#5');
+for (let i = 100; i >= 0; i--){
+    console.log(i);
+}
