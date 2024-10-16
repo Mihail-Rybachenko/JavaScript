803c3a1a224ec43abea8c0f3fe9ef764db5c7cdf
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-16 19:46:56 +0300

    add task 156-158

---
+++ conditions/Mihail156-158task/script.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log('156#1');
+for (let i = 1; i <= 9; i++) {
+	for (let j = 1; j <= 3; j++) {
+		document.write(i);
+	}
+}
+document.write('<p>');
+console.log('156#2');
+for (let i = 1; i <= 3; i++) { 
+	for (let j = 1; j <= 3; j++) {  
+	  document.write(i + "" + j + " ");  
+	}
+  }
