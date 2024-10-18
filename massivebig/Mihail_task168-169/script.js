403b5a2725c078aadb41ea2b0e68142d4134aab9
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-18 12:25:40 +0300

    add task 168-169

---
+++ massivebig/Mihail_task168-169/script.js
@@ -0,0 +1,28 @@
+"use strict";
+console.log('168#1');
+let arr = [
+	['a', 'b', 'c'],
+	['d', 'e', 'f'],
+	['g', 'h', 'i'],
+	['j', 'k', 'l'],
+];
+console.log(arr[3][2] + arr[1][1] + arr[2][0] + arr[0][0]);
+console.log('168#2');
+let arr1 = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+console.log(arr1[0][0] + arr1[0][1] +arr1[1][0] +arr1[1][1] +arr1[2][0] +arr1[2][1]);
+console.log('169#1');
+let arr2 = [
+	[
+		[1, 2],
+		[3, 4],
+	],
+	[
+		[5, 6],
+		[7, 8],
+	],
+];
+console.log(arr2[0][0][0] + arr2[0][0][1]+ arr2[0][1][0]+ arr2[0][1][1] + arr2[1][0][0] + arr2[1][0][1] + arr2[1][1][0] + arr2[1][1][1]);
