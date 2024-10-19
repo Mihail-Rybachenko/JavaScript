0fa9a578fb27096f91a28311d26e7ae399b05362
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-19 20:54:26 +0300

    add task 174

---
+++ massivebig/Mihail_task174/script.js
@@ -0,0 +1,74 @@
+"use strict";
+console.log('174#1');
+let array1 = [];
+
+for (let i = 0; i < 3; i++) {
+    array1[i] = []; 
+    
+    for (let j = 1; j <= 5; j++) {
+        array1[i].push(j); 
+    }
+}
+
+console.log(array1);
+console.log('174#2');
+let array2 = [];
+
+for (let i = 0; i < 3; i++) {
+    array2[i] = []; 
+    
+    for (let j = 1; j <= 5; j++) {
+        array2[i].push(j); 
+    }
+}
+
+console.log(array2);
+console.log('174#3');
+let array3 = [];
+
+for (let i = 0; i < 3; i++) {
+    array3[i] = []; 
+    
+    for (let j = 1; j <= 5; j++) {
+        array3[i].push(j); 
+    }
+}
+
+console.log(array3);
+console.log('174#4');
+let array4 = [];
+
+for (let i = 0; i < 3; i++) {
+    array4[i] = []; 
+    
+    for (let j = 1; j <= 5; j++) {
+        array4[i].push(j); 
+    }
+}
+
+console.log(array4);
+console.log('174#5');
+let array5 = [];
+
+for (let i = 0; i < 3; i++) {
+    array5[i] = []; 
+    
+    for (let j = 1; j <= 5; j++) {
+        array5[i].push(j); 
+    }
+}
+
+console.log(array5);
+
+console.log('174#6');
+let array6 = [];
+
+for (let i = 0; i < 3; i++) {
+    array6[i] = []; 
+    
+    for (let j = 1; j <= 5; j++) {
+        array6[i].push(j); 
+    }
+}
+
+console.log(array6);
