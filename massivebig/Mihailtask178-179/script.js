ac219946a24f123ae7b346fdfb6b141663d41ea4
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-20 20:08:42 +0300

    add task 178-179

---
+++ massivebig/Mihailtask178-179/script.js
@@ -0,0 +1,111 @@
+"use strict";
+console.log('178#1');
+let students = {
+	'group1': ['name11', 'name12', 'name13'],
+	'group2': ['name21', 'name22', 'name23'],
+	'group3': ['name31', 'name32', 'name33'],
+};
+console.log(students['group3'][0]);
+
+console.log('179#1');
+let data1 = {
+	1: [
+		'data11',
+		'data12',
+		'data13',
+	],
+	2: [
+		'data21',
+		'data22',
+		'data23',
+	],
+	3: [
+		'data31',
+		'data32',
+		'data33',
+	],
+	4: [
+		'data41',
+		'data42',
+		'data43',
+	],
+};
+
+for (let key in data1) {
+	for (let value of data1[key]) {
+		console.log(value);
+	}
+}
+
+console.log('179#2');
+
+let data2 = [
+	{
+		1: 'data11',
+		2: 'data12',
+		3: 'data13',
+	},
+	{
+		1: 'data21',
+		2: 'data22',
+		3: 'data33',
+	},
+	{
+		1: 'data31',
+		2: 'data32',
+		3: 'data33',
+	},
+];
+
+for (let obj of data2) {
+	for (let key in obj) {
+		console.log(obj[key]);
+	}
+}
+console.log('179#3');
+let data3 = [
+	{
+		1: [
+			'data111',
+			'data112',
+			'data113',
+		],
+		2: [
+			'data121',
+			'data122',
+			'data123',
+		],
+	},
+	{
+		1: [
+			'data211',
+			'data212',
+			'data213',
+		],
+		2: [
+			'data221',
+			'data222',
+			'data223',
+		],
+	},
+	{
+		1: [
+			'data411',
+			'data412',
+			'data413',
+		],
+		2: [
+			'data421',
+			'data422',
+			'data423',
+		],
+	},
+];
+
+for (let obj of data3) {
+	for (let key in obj) {
+		for (let value of obj[key]) {
+			console.log(value);
+		}
+	}
+}
