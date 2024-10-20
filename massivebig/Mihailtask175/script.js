1158cd543ba19ebbf24a889822252774efb7372e
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-20 19:43:16 +0300

    add task175

---
+++ massivebig/Mihailtask175/script.js
@@ -0,0 +1,183 @@
+"use strict";
+console.log('175#1');
+let arr1 = [];
+let counter1 = 1;
+
+for (let i = 0; i < 4; i++) {
+  arr1[i] = [];
+  
+  for (let j = 0; j < 2; j++) {
+    arr1[i].push(counter1++);
+  }
+}
+
+console.log(arr1);
+
+console.log('175#2');
+let arr2 = [];
+let counter2 = 2;
+
+for (let i = 0; i < 4; i++) {
+  arr2[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr2[i].push(counter2);
+    counter2 += 2;
+  }
+}
+
+console.log(arr2);
+
+console.log('175#3');
+let arr3 = [];
+let counter3 = 1;
+
+for (let i = 0; i < 2; i++) {
+  arr3[i] = [];
+  
+  for (let j = 0; j < 2; j++) {
+    arr3[i][j] = [counter3++, counter3++];
+  }
+}
+
+console.log(arr3);
+
+console.log('175#4');
+let arr4 = [];
+let counter4 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr4[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr4[i][j] = counter4++; 
+  }
+}
+
+console.log(arr4);
+
+console.log('175#5');
+let arr5 = [];
+let counter5 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr5[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr5[i][j] = counter5++;
+  }
+}
+
+console.log(arr5);
+
+console.log('175#6');
+let arr6 = [];
+let counter6 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr6[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr6[i][j] = counter6++;
+  }
+}
+
+console.log(arr6);
+
+console.log('175#7');
+let arr7 = [];
+let counter7 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr7[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr7[i][j] = counter7++;
+  }
+}
+
+console.log(arr7);
+
+console.log('175#8');
+let arr8 = [];
+let counter8 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr8[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr8[i][j] = counter8++;
+  }
+}
+
+console.log(arr8);
+
+console.log('175#9');
+let arr9 = [];
+let counter9 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr9[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr9[i][j] = counter9++;
+  }
+}
+
+console.log(arr9);
+
+console.log('175#10');
+let arr10 = [];
+let counter10 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr10[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr10[i][j] = counter10++;
+  }
+}
+
+console.log(arr10);
+
+console.log('175#11');
+let arr11 = [];
+let counter11 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr11[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr11[i][j] = counter11++;
+  }
+}
+
+console.log(arr11);
+
+console.log('175#12');
+let arr12 = [];
+let counter12 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr12[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr12[i][j] = counter12++;
+  }
+}
+
+console.log(arr12);
+
+console.log('175#13');
+let arr13 = [];
+let counter13 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr13[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr13[i][j] = counter13++;
+  }
+}
+
+console.log(arr13);
