1132cb8a8cdd27cd490be0a94bb5f9a3e7639745
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-26 20:04:22 +0300

    add task223-224

---
+++ types_function/Mihail_223-224/script.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log('223#1');
+function func() {
+	return '!';
+}
+
+console.log(func);
+console.log('224#1-3');
+function func(){
+	return 'gdsaaa';
+}
+console.log(func())
+console.log(func)
+console.log('224#4');
+func = 123;
+console.log(func);
