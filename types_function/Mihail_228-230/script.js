3bf4cccd35c362fe94f21846cd0d111576f0b606
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-26 20:26:31 +0300

    add task 228-230

---
+++ types_function/Mihail_228-230/script.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log('228#1');
+console.log('Function Declaration');
+console.log('228#2');
+console.log('Function Expression');
+console.log('229#1');
+Name();
+function Name(){
+	console.log('Misha');
+}
+console.log('229#2');
+fun();
+let fun = function(){
+	console.log('sound');
+}
+console.log('230#1');
+let func1 = function() {console.log('!')};
+let func2 = function() {
+	console.log('!')
+};
+function func3() {
+	console.log('!')
+}
