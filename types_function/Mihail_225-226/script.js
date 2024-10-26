d851d7cb3e05d1b24fafdda71f0b17044b08408a
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-26 20:12:51 +0300

    add task 225-227

---
+++ types_function/Mihail_225-226/script.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log('225#1-3');
+function func1() {
+	return 3;
+};
+let func2 = func1;
+console.log(func1() + func2());
+console.log('226#1-3');
+let func3 = function(){
+	return 1;
+}
+let func4 = function(){
+	return 3;
+}
+console.log(func3() + func4());
