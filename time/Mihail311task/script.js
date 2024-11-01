f7cb5f799c2fb817797b97c458bab72ae94d3db4
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-01 21:01:32 +0300

    add task 311

---
+++ time/Mihail311task/script.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('311#1');
+let date = new Date();
+
+console.log(date.getFullYear()); // год
+console.log(date.getMonth() + 1);    // месяц
+console.log(date.getDate());     // день
