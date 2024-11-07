f85de3adeb8cf327d77cbd787868348bfeacb559
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-07 19:22:47 +0300

    add task 341

---
+++ DOM/Mihailtask341/script.js
@@ -0,0 +1,13 @@
+"use strict";
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+button1.addEventListener('click', function() {
+	alert('1');
+});
+button2.addEventListener('click', function() {
+	alert('2');
+});
+button3.addEventListener('click', function() {
+	alert('3');
+});
