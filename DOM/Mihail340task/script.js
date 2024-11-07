e3c110cb210e5ee6923c04bbca0df2cd0c5a4a51
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-07 19:14:14 +0300

    add task340

---
+++ DOM/Mihail340task/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let block = document.querySelector('#block p');
+console.log(block);
+let block1 = document.querySelector('.block p');
+console.log(block1);
+let www = document.querySelector('.www');
+console.log(www);
