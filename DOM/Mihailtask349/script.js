b1ae49ce6f0e780fd9ff9799d08727555a35f024
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-11 19:22:12 +0300

    add task 349

---
+++ DOM/Mihailtask349/script.js
@@ -0,0 +1,59 @@
+// №1: Записать текст в инпут
+let elem = document.querySelector('#elem');
+let btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', () => {
+  elem.value = 'new text'; // Заменить текст в инпуте
+});
+
+// №2: Записать текст из инпута в абзац
+let inputElem = document.querySelector('#inputElem');
+let outputElem = document.querySelector('#outputElem');
+let btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+  outputElem.textContent = inputElem.value; // Записать текст в абзац
+});
+
+// №3: Записать квадрат числа из первого инпута во второй инпут
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+let btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', () => {
+  let number = Number(input1.value); // Получаем число из первого инпута
+  input2.value = number * number; // Записываем квадрат во второй инпут
+});
+
+// №4: Поменять значения местами
+let input3 = document.querySelector('#input3');
+let input4 = document.querySelector('#input4');
+let btn4 = document.querySelector('#btn4');
+btn4.addEventListener('click', () => {
+  let temp = input3.value;
+  input3.value = input4.value;
+  input4.value = temp; // Меняем значения местами
+});
+
+// №5: Записать среднее арифметическое чисел из 5 инпутов
+let inputs = [
+  document.querySelector('#input5'),
+  document.querySelector('#input6'),
+  document.querySelector('#input7'),
+  document.querySelector('#input8'),
+  document.querySelector('#input9')
+];
+let resultElem = document.querySelector('#result');
+let btn5 = document.querySelector('#btn5');
+btn5.addEventListener('click', () => {
+  let sum = 0;
+  let count = 0;
+  
+  inputs.forEach(input => {
+    let value = Number(input.value);
+    if (!isNaN(value)) { // Проверка, что введено число
+      sum += value;
+      count++;
+    }
+  });
+  
+  let average = count > 0 ? sum / count : 0;
+  resultElem.textContent = `Среднее арифметическое: ${average}`; // Отображаем среднее арифметическое
+});
