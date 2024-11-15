733f128336976c4b53bd4756439f45efbf350d11
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-15 13:36:34 +0300

    add task 365

---
+++ atributs/Mihailtask365/script.js
@@ -0,0 +1,8 @@
+let elem = document.querySelector('#elem');  
+let value = elem.getAttribute('value');      
+
+console.log(value);  
+let elem1 = document.querySelector('#elem');  
+let classValue = elem1.getAttribute('class');  
+
+console.log(classValue);  
