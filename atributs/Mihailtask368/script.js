7a6ead147cf94107220c7af81280fb5102fd6c3e
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-15 13:42:14 +0300

    add task 368

---
+++ atributs/Mihailtask368/script.js
@@ -0,0 +1,7 @@
+let elem = document.querySelector('#elem');
+
+if (elem.hasAttribute('value')) {
+    console.log('Атрибут value существует.');
+} else {
+    console.log('Атрибут value не существует.');
+}
