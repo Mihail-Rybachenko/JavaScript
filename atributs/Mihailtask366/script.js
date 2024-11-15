f8bcdb2c7e2dd94b460cbb68bc09dab78f431060
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-15 13:38:40 +0300

    add task 366

---
+++ atributs/Mihailtask366/script.js
@@ -0,0 +1,8 @@
+let elem = document.querySelector('#elem');  
+elem.setAttribute('value', 'text');            
+
+console.log(elem.getAttribute('value'));      
+let elem1 = document.querySelector('#elem');  
+elem.setAttribute('class', 'valid');           
+
+console.log(elem1.getAttribute('class'));      
