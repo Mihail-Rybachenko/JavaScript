3ed8472f6ee96c3ed4ab808173f0832e198acc47
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-22 19:28:41 +0300

    add task 403

---
+++ Forms/Mihailtask403/script.js
@@ -0,0 +1,17 @@
+
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+
+
+input1.addEventListener('input', function () {
+    if (this.value.length === 2) {
+        input2.focus(); 
+    }
+});
+
+
+input2.addEventListener('input', function () {
+    if (this.value.length === 2) {
+        this.blur(); 
+    }
+});
